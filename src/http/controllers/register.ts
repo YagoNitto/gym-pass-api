8fc7117b22c9cd230c6b61fa77e3ca505a0cb904
yagonitto <yago.nitto@outlook.com>
2026-07-28 16:48:31 -0300

    feat: add bcrypt for hash and validation if user already registered

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcryptjs'
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod/v4'
 import { prisma } from '@/lib/prisma.js'
@@ -11,11 +12,23 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const passwordHash = await hash(password, 8)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
   const user = await prisma.user.create({
     data: {
       name,
       email,
-      passwordHash: password,
+      passwordHash,
     },
   })
 
