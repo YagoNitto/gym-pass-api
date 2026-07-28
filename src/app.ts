4c31dcb6809839f8248d38dbf303fff9a8b63c87
yagonitto <yago.nitto@outlook.com>
2026-07-28 14:11:27 -0300

    feat: initial create user endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
 import fastify from 'fastify'
+import { z } from 'zod/v4'
+import { prisma } from './lib/prisma.js'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      passwordHash: password,
+    },
+  })
+
+  return reply.status(201).send({ id: user.id })
+})
