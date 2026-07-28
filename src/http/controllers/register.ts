e9b20e32245186513bb028c0cd7a35d516396e78
yagonitto <yago.nitto@outlook.com>
2026-07-28 17:43:31 -0300

    feat: add user already exists error

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod/v4'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.js'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error.js'
 import { RegisterUseCase } from '@/use-cases/register.js'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -17,8 +18,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerUseCase = new RegisterUseCase(usersRepository)
 
     await registerUseCase.execute({ name, email, password })
-  } catch {
-    return reply.status(409).send()
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ error: err.message })
+    }
+
+    return reply.status(500).send()
   }
 
   return reply.status(201).send()
