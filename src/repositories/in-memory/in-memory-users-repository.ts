9a3987dd56cecd3b33f74ef8e24ab4baeb60958f
yagonitto <yago.nitto@outlook.com>
2026-07-29 11:29:56 -0300

    test: add in memory database repository to tests

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,31 @@
+import type { User } from '@/generated/prisma/client.js'
+import type { UserCreateInput } from '@/generated/prisma/models.js'
+import type { UsersRepository } from '../users-repository.js'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async create(data: UserCreateInput): Promise<User> {
+    const user = {
+      id: 'user-01',
+      name: data.name,
+      email: data.email,
+      passwordHash: data.passwordHash,
+      createdAt: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find(item => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+}
